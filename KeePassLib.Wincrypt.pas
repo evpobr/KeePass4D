736eadb863176716243d35957cc60e641251783f
evpobr <evpobr@gmail.com>
2017-01-29 18:24:05 +0500

    Add license text to units

--- KeePassLib.Wincrypt.pas
+++ KeePassLib.Wincrypt.pas
@@ -1,3 +1,18 @@
+{   This file is part of KeePass4D.                                       
+
+    KeePass4D is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    KeePass4D is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with KeePass4D.  If not, see <http://www.gnu.org/licenses/>. }
+
 unit KeePassLib.Wincrypt;
 
 {$ALIGN ON}
